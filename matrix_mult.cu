a03fc4fd2e1f2930aa4f80a9e98caa74da92d3ce
MurphLaws <lasso.nicolas@correounivalle.edu.co>
2023-11-27 14:29:09 +0100

    Added MatrixAdd

--- matrix_mult.cu
+++ matrix_mult.cu
@@ -18,27 +18,48 @@ void MatrixPrint(float **M, int n, int p) {
     printf("\n");
 }
 
+void MatrixAdd(float **M1, float **M2, float **Mout, int n, int p) {
+    int i, j;
+    for (i = 0; i < n; i++)
+        for (j = 0; j < p; j++)
+            Mout[i][j] = M1[i][j] + M2[i][j];
+}
+
+
+
 int main() {
-    float **M;
+    float **M, **M1, **M2, **Mout;
     int n = 3, p = 4;
 
-    // Allocate memory for the 2D array
     M = (float **)malloc(n * sizeof(float *));
-    for (int i = 0; i < n; ++i) {
+    M1 = (float **)malloc(n * sizeof(float *));
+    M2 = (float **)malloc(n * sizeof(float *));
+    Mout = (float **)malloc(n * sizeof(float *));
+
+    for (int i = 0; i < n; i++) {
         M[i] = (float *)malloc(p * sizeof(float));
+        M1[i] = (float *)malloc(p * sizeof(float));
+        M2[i] = (float *)malloc(p * sizeof(float));
+        Mout[i] = (float *)malloc(p * sizeof(float));
     }
 
-    // Initialize the matrix
-    MatrixInit(M, n, p);
+    MatrixInit(M1, n, p);
+    MatrixInit(M2, n, p);
 
-    // Print the matrix
-    MatrixPrint(M, n, p);
+    MatrixAdd(M1, M2, Mout, n, p);
 
-    // Free the allocated memory
-    for (int i = 0; i < n; ++i) {
+    MatrixPrint(M1, n, p);
+    MatrixPrint(M2, n, p);
+    MatrixPrint(Mout, n, p);
+
+ 
+
+    for (int i = 0; i < n; i++) {
         free(M[i]);
+        free(M1[i]);
+        free(M2[i]);
+        free(Mout[i]);
     }
-    free(M);
 
     return 0;
 }
